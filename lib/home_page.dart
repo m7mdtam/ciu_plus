a4d7eec1909847a4cb222f8889cb73bd808aa9af
m7mdtam <mohammedtamimi72@gmail.com>
2024-04-30 01:43:48 +0300

    adding drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -13,9 +13,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 109, 129, 149),
         title: Text(
           'CIU Plus ',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(255, 135, 229, 229),
+              ),
+              child: Text(
+                'Welcome mohammed',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.account_box,
+                size: 40,
+              ),
+              title: Text(
+                'Profile',
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ],
         ),
       ),
     );
